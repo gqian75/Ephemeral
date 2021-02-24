6a1b90130e28a99b0ba1c952cfcdaef7d035950b
Khue Tran <khuetran@utexas.edu>
2021-02-23 20:55:49 -0600

    flask

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/albums/')
+def albums():
+    return render_template('albums.html')
+
+@app.route('/artists/')
+def artists():
+    return render_template('artists.html')
+
+@app.route('/songs/')
+def songs():
+    return render_template('songs.html')
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
