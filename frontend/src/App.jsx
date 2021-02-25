334ccfc3da74091a196b7877182d33f67ac23daf
Kanishak Joshi <kanishakj24@utexas.edu>
2021-02-24 20:58:23 -0600

    changed main.py to GCP specifications

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import useAxios from 'axios-hooks'
+
+function App() {
+
+const[{data}]= useAxios("/api/test");
+
+
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React {data}
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, send_from_directory
+from flask_cors import CORS
+app = Flask(__name__, static_folder="./frontend/build/static", template_folder="./frontend/build")
+CORS(app)
 
-app = Flask(__name__)
+
+# Part of the lecture video but we are not using it currently. 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+  return render_template("index.html")
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/api/test")
+def test():
+      return "TEST123456789"
+
+
+
+
+
+# Index render that we did but commented because it's differnt from lecture video 
+# 
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 @app.route('/about/')
 def about():
@@ -23,6 +42,7 @@ def artists():
 def songs():
     return render_template('songs.html')
 
+
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    app.run(host='0.0.0.0', port=80, threaded=True, debug=True)
+    # app.run()
