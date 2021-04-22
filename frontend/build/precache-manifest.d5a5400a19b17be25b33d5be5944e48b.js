a428b3c5b7a2c16126fff5b4f40e6368fdb11e34
kanishakj24 <kanishakj24@utexas.edu>
2021-04-22 18:50:21 +0000

    Phase1 changes

---
+++ frontend/build/precache-manifest.d5a5400a19b17be25b33d5be5944e48b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa62814be7673953b1a18d54cdc0ba1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f35836b3dfc4331b5af9",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "4139db4cc38e1f1a0f30",
+    "url": "/static/js/2.3d425ccd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3d425ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f35836b3dfc4331b5af9",
+    "url": "/static/js/main.65b3c69c.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
