db5aced38316346e49f7b2208705804a0c25b833
Jake Goins <jakegoins@utexas.edu>
2021-04-22 12:31:49 -0500

    Commented out format_lists

--- main.py
+++ main.py
@@ -29,82 +29,92 @@ album_list = query.fetchall()
 query.close()
 db.close()
 
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-  return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route("/api/test")
 def test():
-      return "TEST123456789"
+    return "TEST123456789"
+
 
 @app.route('/about/')
 def about():
     return render_template('about.html')
 
+
 @app.route('/albums/')
 def albums():
-    return render_template('albums.html',albums=album_list)
+    return render_template('albums.html', albums=album_list)
+
 
 @app.route('/artists/')
 def artists():
-    
-    return render_template('artists.html',artists=artist_list)
+    return render_template('artists.html', artists=artist_list)
+
 
 @app.route('/songs/')
 def songs():
-    return render_template('songs.html',songs=song_list)
+    return render_template('songs.html', songs=song_list)
 
-#Dynamic instances of different types
+
+# Dynamic instances of different types
 def id(string):
-    string = string.replace(" ","-").lower().strip()
-    string = string.replace("'","")
-    string = string.replace("/","")
+    string = string.replace(" ", "-").lower().strip()
+    string = string.replace("'", "")
+    string = string.replace("/", "")
     return string
 
+
 def find(name, data, list_type):
-    
     for list_item in data:
-        if(list_type == "album"):
-            if(list_item.album_id == name):
+        if (list_type == "album"):
+            if (list_item.album_id == name):
                 return list_item
-        elif(list_type == "artist"):
-            if(list_item.artist_id == name):
+        elif (list_type == "artist"):
+            if (list_item.artist_id == name):
                 return list_item
-        elif(list_type == "song"):
-            if(list_item.song_id == name):
+        elif (list_type == "song"):
+            if (list_item.song_id == name):
                 return list_item
     return None
 
+
 @app.route('/albums/<string:_album>')
 def album(_album):
-    #_album = id(_album)
-    #album_list.get(_album,None)
-    return render_template('album.html', album=find(_album,album_list,'album'))
+    # _album = id(_album)
+    # album_list.get(_album,None)
+    return render_template('album.html', album=find(_album, album_list, 'album'))
+
 
 @app.route('/artists/<string:_artist>')
 def artist(_artist):
-    #_artist = id(_artist)
-    return render_template('artist.html', artist=find(_artist,artist_list,'artist'))
+    # _artist = id(_artist)
+    return render_template('artist.html', artist=find(_artist, artist_list, 'artist'))
+
 
 @app.route('/songs/<string:_song>')
 def song(_song):
-    #_song = id(_song)
-    return render_template('song.html', song=find(_song,song_list,'song'))
+    # _song = id(_song)
+    return render_template('song.html', song=find(_song, song_list, 'song'))
+
 
 def format_strings(string):
     new_string = ""
     for char in string:
         if (char == "{" or char == "}" or char == '"'):
             new_string += ""
-            
+
         elif (char == ","):
             new_string += ", "
         else:
             new_string += char
     new_string = new_string.title()
     return new_string
+
 
 def format_lists():
     for artist in artist_list:
@@ -113,18 +123,16 @@ def format_lists():
         album.album_genre = format_strings(album.album_genre)
         print(album.album_genre)
     for song in song_list:
-        seconds = str(int((int(song.duration)//1000.0)%60))
-        song.duration = str(int((int(song.duration)//1000.0)//60)) + ":"
-        if(len(seconds) == 1):
+        seconds = str(int((int(song.duration) // 1000.0) % 60))
+        song.duration = str(int((int(song.duration) // 1000.0) // 60)) + ":"
+        if (len(seconds) == 1):
             song.duration += "0" + seconds
         else:
             song.duration += seconds
-        
+
 
 if __name__ == "__main__":
-    format_lists()
-    app.jinja_env.globals.update(id=id) 
-    #app.run(host='0.0.0.0', port=80, threaded=True)
+    # format_lists()
+    app.jinja_env.globals.update(id=id)
+    # app.run(host='0.0.0.0', port=80, threaded=True)
     app.run()
-    
-   
