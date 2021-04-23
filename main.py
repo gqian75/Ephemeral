7c86a3b517d732447076b95638fb809c6ba02be8
Jake Goins <jakegoins@utexas.edu>
2021-04-23 10:48:03 -0500

    Fix for frontend. Albums still needs work

--- main.py
+++ main.py
@@ -17,13 +17,13 @@ DB_PASS = "gUj52EfnqxD2UeVkSPQ7kDeiAQOwMcu6"
 db = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 query = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-query.execute('SELECT * FROM song;')
+query.execute('SELECT * FROM song order by song.rank;')
 song_list = query.fetchall()
 
-query.execute('SELECT * FROM artist;')
+query.execute('select * from artist left join link on artist.artist_rank = link.artist_rank left join song on song.rank = link.song_rank;')
 artist_list = query.fetchall()
 
-query.execute('SELECT * FROM album;')
+query.execute("select * from album left join song on album.album_rank = song.album_rank order by album.album_rank;")
 album_list = query.fetchall()
 
 query.close()
